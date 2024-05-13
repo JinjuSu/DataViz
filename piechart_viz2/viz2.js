4071788a78bbedae15457ad7d84a5b54d36ec1d6
DarrellD234 <147477258+DarrellD234@users.noreply.github.com>
2024-05-13 14:54:19 +1000

    Add files via upload
    Pie Chart (Visualization 2 - In Progress)

---
+++ piechart_viz2/viz2.js
@@ -0,0 +1,16 @@
+function init() {
+    //Width and Height of the SVG container
+    var w = 500;
+    var h = 400;
+
+    //SVG attributes
+    var svg = d3.select("#pie-chart")
+                .append("svg")
+                .attr("width", w)
+                .attr("height", h)
+                .style("background-color", "grey"); 
+            
+
+}
+
+window.onload = init;
